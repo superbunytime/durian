28e67b55977c42f183f60bf26450dbcae89a9fbe
Eric White <led.ecodyswhite@gmail.com>
2023-10-01 16:21:37 -0700

    apicalltest is RIGHT there but not done yet

--- apicalltests.js
+++ apicalltests.js
@@ -2,10 +2,12 @@ import axios, * as others from "axios";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const fs = require("fs");
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 import dotenv from "dotenv";
 
 dotenv.config();
 const token = process.env.USA_JOBS_API_KEY;
+const jooble_token = process.env.JOOBLE_API_KEY;
 
 const request = require("request");
 
@@ -13,31 +15,64 @@ const host = "data.usajobs.gov";
 const userAgent = "led.ecodyswhite@gmail.com";
 const authKey = token;
 
-request(
-  {
-    url: "https://data.usajobs.gov/api/search?JobCategoryCode=2210&Keyword=Software Development&LocationName=Washington, DC",
-    method: "GET",
-    headers: {
-      Host: host,
-      "User-Agent": userAgent,
-      "Authorization-Key": authKey,
-    },
-  },
-  function (error, response, body) {
-    let data = JSON.parse(body);
-    console.log(
-      data.SearchResult.SearchResultItems[0].MatchedObjectDescriptor.PositionURI
-    );
-    for (let i = 0; i < 10; i++) {
-      console.log(
-        data.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
-          .PositionURI
-      );
-    }
-    // doesn't work???
-    // i suspect this does not work because i'm incrementing the wrong thing
-    // no that's not it
+// USAJOBS API CALL TESTS
 
-    // LMAO I FORGOT FOR LOOP SYNTAX
+// request(
+//   {
+//     url: "https://data.usajobs.gov/api/search?JobCategoryCode=2210&Keyword=Software Development&LocationName=Washington, DC",
+//     method: "GET",
+//     headers: {
+//       Host: host,
+//       "User-Agent": userAgent,
+//       "Authorization-Key": authKey,
+//     },
+//   },
+//   function (error, response, body) {
+//     let data = JSON.parse(body);
+//     console.log(
+//       data.SearchResult.SearchResultItems[0].MatchedObjectDescriptor.PositionURI
+//     );
+//     for (let i = 0; i < 10; i++) {
+//       console.log(
+//         data.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
+//           .PositionURI
+//       );
+//     }
+//   }
+// );
+
+// JOOBLE API CALL TESTS
+
+const jooble_url = "https://jooble.org/api/";
+const jooble_key = jooble_token;
+const jooble_params = "{ keywords: 'software', location: 'Remote'}";
+
+//create xmlHttpRequest object
+const http = new XMLHttpRequest();
+//open connection. true - asynchronous, false - synchronous
+http.open("POST", jooble_url + jooble_key, true);
+
+//Send the proper header information
+http.setRequestHeader("Content-type", "application/json");
+
+//Callback when the state changes
+http.onreadystatechange = function () {
+  if (http.readyState == 4 && http.status == 200) {
+    // console.log(http.responseText);
+    // for (let i = 0; i < 10; i++){
+    //   console.log(JSON.parse(http.responseText))
+    //spelling the variable 'responsText' isn't going to give me
+    //the results i'm looking for
+    //i think i need to use JSON.parse
+    // }
+    console.log(JSON.parse(http.responseText));
+    // ??? ???????
+    // data = JSON.parse(http.responseText);
   }
-);
+};
+//Send request to the server
+// http.send(jooble_params);
+
+
+let obj = {"totalCount": 1, "jobs": ["foo"]}
+console.log(obj.jobs[0])
