3f195327239bae5d3895be8bc802e8899818db5e
Eric White <led.ecodyswhite@gmail.com>
2023-10-03 18:20:28 -0700

    trying to get the preload abstracted from app.js

--- durian-react-app/src/App.js
+++ durian-react-app/src/App.js
@@ -1,32 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./duriantransparent.png";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header
-        className="App-header"
-        onClick={() => {
-          document
-            .getElementsByClassName("App-logo")[0]
-            .style.setProperty("display", "none");
-          document
-            .getElementsByClassName("dIsCome")[0]
-            .style.setProperty("display", "none");
-          document
-            .getElementsByClassName("App-header")[0]
-            .style.setProperty("display", "none");
-        }}
-      >
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="dIsCome">Durian is coming...</p>
-      </header>
-      <body>
-        <p>hey here's some body text</p>
-      </body>
-    </div>
-  );
+  return <preload />
 }
 
 export default App;
