96c72f22831749fc0b9a16ced4b9ac9a2bfe8d90
Eric White <led.ecodyswhite@gmail.com>
2023-10-04 13:55:37 -0700

    got forms put in, got them to make the app crash

--- durian-react-app/src/App.js
+++ durian-react-app/src/App.js
@@ -1,30 +1,18 @@
 import React, { useState } from "react";
 import logo from "./duriantransparent.png";
 import "./App.css";
+import SearchForm from "./SearchForm.js";
 
 function App() {
   return (
     <div className="App">
-      <header
-        className="App-header"
-        onClick={() => {
-          document
-            .getElementsByClassName("App-logo")[0]
-            .style.setProperty("display", "none");
-          document
-            .getElementsByClassName("dIsCome")[0]
-            .style.setProperty("display", "none");
-          document
-            .getElementsByClassName("App-header")[0]
-            .style.setProperty("display", "none");
-        }}
-      >
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="dIsCome">Durian is coming...</p>
+        <p>
+          Durian is coming...
+        </p>
       </header>
-      <body>
-        <p>hey here's some body text</p>
-      </body>
+      <SearchForm/>
     </div>
   );
 }
---
+++ durian-react-app/src/SearchForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+function SearchForm() {
+  const [formData, setFormData] = useState({
+    jobSearch: "", // title, keywords, company
+    location: "", // blank for anywhere
+  });
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData((formData) => ({
+      ...formData,
+      [name]: value,
+    }));
+  };
+
+  const gatherInput = (evt) => {
+    evt.preventDefault();
+    // this is where we'll call a function to parse the data
+    // based off the search form inputs
+    console.log(formData)
+    setFormData({ jobSearch: "", location: "" });
+    // returning the search form back to default empty string
+    // don't necessarily need to do that but here's how it's done.
+  };
+
+  // need to change the return value because it's gonna render stuff that's not referenced
+  // it's literal copy-paste from the boxes assignment
+  return (
+    <div>
+    <form onSubmit={gatherInput}>
+      <div>
+        <label htmlFor="jobSearch">Job Search</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="jobSearch"
+          value={formData.jobSearch}
+          id="height"
+        />
+      </div>
+      <div>
+        <label htmlFor="location">Location</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="location"
+          id="location"
+          value={formData.location}
+        />
+      </div>
+    </form>
+    <button id = "searchButton" onClick={this.gatherInput}>Search</button>
+  </div>
+);
+ // line 52, the search button makes it crash because that's not been properly implemented.
+}
+
+export default SearchForm;
