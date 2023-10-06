d96764ce9bb5072ae63ff63377385c671056546f
Eric White <led.ecodyswhite@gmail.com>
2023-10-05 23:35:02 -0700

    search results are in the console for now

--- durian-react-app/src/RenderSearch.js
+++ durian-react-app/src/RenderSearch.js
@@ -3,9 +3,16 @@ import JOOBLE_data from './JOOBLE.json'
 import USA_JOBS_data from './USA_JOBS.json'
 
 function RenderSearch(){
+
+
+    const j_parse = JSON.stringify(JOOBLE_data)
+    const u_parse = JSON.stringify(USA_JOBS_data)
+    console.log(j_parse)
+    console.log(u_parse)
+
     return(
         <div>
-            {{JOOBLE_data}}{{USA_JOBS_data}}
+            Check console for job searches.
         </div>
     )
 }
